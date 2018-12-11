de41307307977ef51495451f4f56c4859f79514c
Erick Mejia <erick.mejia.au@gmail.com>
2018-12-11 17:47:15 +1000

    Add project files.

---
+++ VidRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VidRental.Startup))]
+namespace VidRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
